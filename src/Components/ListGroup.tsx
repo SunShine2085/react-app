aaf702b91cd71acb9392fa856b06bba21bdfa58a
Chris Courville <ccourville80@gmail.com>
2024-06-02 23:00:44 -0400

    working

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -9,20 +9,18 @@ function ListGroup() {
     ];
     items = [];
 
-    const getMessage = () => {
-        return items.length === 0 ? <p>No item found</p>: null;; 
-    }
+    
 
     return (
         <>
             <h1>List</h1>
-            {getMessage()}
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
                 {items.map((item) => (<li key={item}>{item}</li>))}
 
             </ul>
         </>
     );
-}
 
+}
 export default ListGroup;
