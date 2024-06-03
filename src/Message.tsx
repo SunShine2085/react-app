d7f065dbfd6a4a26353512f166a5ae0622e50d6b
Chris Courville <ccourville80@gmail.com>
2024-06-02 22:46:07 -0400

    Initial

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//PascalCasing//
+function Message(){ 
+    const name = 'Chris, you super smart cool guy!';
+    return <h1>Hello {name}</h1>
+    //JSX: JavaScript XML
+    
+}
+export default Message;
+
