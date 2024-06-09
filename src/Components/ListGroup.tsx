fba6bc2bcb70f72f763680c1ffe779422e0671e8
Chris Courville <136990672+SunShine2085@users.noreply.github.com>
2024-06-09 13:59:49 -0400

    working

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,27 +1,25 @@
 
-function ListGroup() {
-    let items = [
-        'New York',
-        'San Fransisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+import{ useState} from "react";
 
+interface ListGroupProps{
+    items: string[];
+    heading: string;
+}
 
+function ListGroup({items, heading}: Props) {
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
 
     return (
         <>
-            <h1>List</h1>
-            {items.length === 0 && <p>No item found</p>}
+            <h1>{heading}</h1>
+            {props.items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
                 {items.map((item,index) => (
                     <li
-                        className="list-group-item"
+                        className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
                         key={item}
-                        onClick={(event) => console.log(event)}
-                    >
+                        onClick={() => { setSelectedIndex(index);}}                    >
                         {item}
                     </li>
                 ))}
