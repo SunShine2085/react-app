8da19418a20fc227ec06d6b4ad0ac88ceb69c713
Chris Courville <136990672+SunShine2085@users.noreply.github.com>
2024-06-09 17:03:12 -0400

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ListGroup from './Components/ListGroup';
+import Alert from "./Components/Alert";
 
 function App(){
   let items = [
@@ -8,10 +9,22 @@ function App(){
     'London',
     'Paris'
 ];
-  return (<div>
-    <ListGroup items={items} heading="Cities"/>
-    </div>
-  );
-}
 
-export default App; 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+  return (<div>
+    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+    <Alert>
+      Hello <span>World</span>
+    </Alert> 
+       </div>
+  );
+  export default App; 
+
+ 
+
+  
+
+
+
